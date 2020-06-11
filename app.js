e30d2e5bacf03d92ea6f359cde2c5c8cfd8b9b7d
Hugh <det.nosnibor00@gmail.com>
2020-06-11 09:46:56 -0500

    Part-one done!

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
   var myImg = document.createElement('img');
   myImg.src = "https://placekitten.com/96/139"; //as of 26 May 2020 cannot do it with gif for security reasons: https://www.chromestatus.com/feature/5629709824032768, changed picture
   myImg.alt = "A Simplsons character waving in front of lockers."
+  
+  var array = ['make coffee', 'eat donut', 'change diapers', 'drive to work']
+  var unorderedTodo = document.createElement('ul');
 
-  var unorderedTodo = document.createElement('ul')
 
   greeting.innerText = 'Hello World!';
 
@@ -18,4 +20,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   greeting.appendChild(myImg);
 
+  document.body.appendChild(unorderedTodo);
+  unorderedTodo.setAttribute("class", "todo-items");
+
+  for (i = 0; i < array.length; i++) {
+    let newLi = document.createElement('li');
+    newLi.innerHTML = array[i];
+    console.log(newLi);
+    unorderedTodo.appendChild(newLi);
+  }
+  console.log(unorderedTodo);
+
+
 });
+
+/*
+Go through the array 
+['make coffee','eat donut','change diapers','drive to work']
+and create an li element for each item e.g. 
+<li>make coffee</li>
+*/
