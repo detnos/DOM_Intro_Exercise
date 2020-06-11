249523b9145d0c949ace188835d29061028a6cc3
Hugh <det.nosnibor00@gmail.com>
2020-06-10 19:35:30 -0500

    implemented some svaing before dinner

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Javascript is alive!");
+window.addEventListener('DOMContentLoaded', (event) => {
+  console.log("Javascript is alive!");
+  var greeting = document.getElementById('greeting');
+  var allListItems = document.getElementsByTagName('li');
+
+  var myImg = document.createElement('img');
+  myImg.src = "https://placekitten.com/96/139"; //as of 26 May 2020 cannot do it with gif for security reasons: https://www.chromestatus.com/feature/5629709824032768, changed picture
+  myImg.alt = "A Simplsons character waving in front of lockers."
+
+  var unorderedTodo = document.createElement('ul')
+
+  greeting.innerText = 'Hello World!';
+
+  for (var i in allListItems) {
+    var listItem = allListItems[i];
+    listItem.style = 'background-color : yellow';
+  }
+
+  greeting.appendChild(myImg);
+
+});
